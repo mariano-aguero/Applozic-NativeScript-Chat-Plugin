4ecdfac4992872b9677cfe3024bf48f8a27f7346
Brad Martin <bradwaynemartin@gmail.com>
2017-10-16 19:22:00 -0500

    clean up repo with gitignore

--- demo/platforms/android/src/main/assets/app/tns_modules/nativescript-applozic-chat/applozic-chat.d.ts
+++
@@ -1,9 +0,0 @@
-import { Common } from './applozic-chat.common';
-export declare class ApplozicChat extends Common {
-    login(alUser: any, successCallback: any, errorCallback: any): void;
-    launchChat(): void;
-    launchChatWithUserId(userId: any): void;
-    launchChatWithGroupId(groupId: number): void;
-    logout(successCallback: any, errorCallback: any): void;
-    showAllRegisteredUsers(showAll: boolean): void;
-}
--- demo/platforms/ios/demo/app/tns_modules/nativescript-applozic-chat/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Common } from './applozic-chat.common';
-export declare class ApplozicChat extends Common {
-  // define your typings manually
-  // or..
-  // take the ios or android .d.ts files and copy/paste them here
-}
